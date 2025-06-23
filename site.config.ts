9419ac23e1d85dafb3f5a219a571ee200e4bcf58
ashish-sai-ou <139072290+ashish-sai-ou@users.noreply.github.com>
2025-06-23 14:39:33 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '62084ae479e74186b975a0eaf8833910',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ashish Sai',
+  domain: 'ashish.nl',
+  author: 'Ashish Sai',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Ashish Sai',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
